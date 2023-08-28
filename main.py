932a6d28cfd37253eee7b960a4a0b59a8d6faaa8
calcalayy <minelandsmc14@gmail.com>
2023-08-28 15:45:21 +0100

    add license

--- main.py
+++ main.py
@@ -29,16 +29,16 @@ async def shlexhangout(ctx):
     embed.set_image(url="https://i.vgy.me/61x9Oy.png")
     await ctx.respond(embed=embed)
 
-@bot.slash_command(description='suggest ton features!', ephemeral=True, guild_ids=['1144777754894676081'])
+@bot.slash_command(description='suggest ton features!', guild_ids=['1144777754894676081'])
 async def suggest(ctx, suggestion):
     embed = discord.Embed(
-        title=f"New Suggestion by {ctx.author}",
+        title=f"New Suggestion by {ctx.author}.",
         description=suggestion,
         color=2829617
     )
     channel = bot.get_channel(1145716515006578862)
     msg = await channel.send(embed=embed)
-    await ctx.respond("Suggestion sent!")
+    await ctx.respond("Suggestion sent!", ephemeral=True)
     await msg.add_reaction("👍")
     await msg.add_reaction("👎")
 
