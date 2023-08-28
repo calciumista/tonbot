be3e0ee7b584443ca115101002765050f4b55826
calcalayy <minelandsmc14@gmail.com>
2023-08-28 15:24:08 +0100

    change desc

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ async def shlexhangout(ctx):
     embed.set_image(url="https://i.vgy.me/61x9Oy.png")
     await ctx.respond(embed=embed)
 
-@bot.slash_command(description='sirius is not a hangout!!!', guild_ids=['1144777754894676081'])
+@bot.slash_command(description='suggest ton features!', guild_ids=['1144777754894676081'])
 async def suggest(ctx, suggestion):
     embed = discord.Embed(
         title=f"New Suggestion by {ctx.author}",
